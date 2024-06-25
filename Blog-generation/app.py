f52c02d01cade41de93952875bfb8c306d4f1006
Gaurav Mohol <gaurav.github@gmail.com>
2024-06-25 23:23:13 +0530

    blog generation using llama-7b model

--- Blog-generation/app.py
+++ Blog-generation/app.py
@@ -1,9 +1,28 @@
 import streamlit as st
 from langchain.prompts import PromptTemplate
-from langchain.llms import ctransformers
+from langchain.llms import CTransformers
 
 # Blog generation code
+def getllamaresponse(input_text, no_words, blog_style):
 
+    # load model using ctransfromers
+    model = CTransformers(model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
+                          model_type='llama',
+                          config={'max_new_tokens':256,
+                                  'temperature':0.01 })
+    # create template 
+    template = """ write blog for {blog_style} job proflie for topic{input_text} withing {no_words} words"""
+
+    prompt = PromptTemplate(input_variables=['blog_style','input_text','no_words'],
+                            template=template)
+    
+    # get response
+    response = model(prompt.format(blog_style = blog_style,
+                                   input_text=input_text,
+                                   no_words= no_words))
+    print(response)
+    return response
+    
 
 st.set_page_config(page_title='Blog Generation',
                    page_icon=':robot_face:',
@@ -23,5 +42,11 @@ with col2:
 
 # add button 
 submit = st.button('submit')
+
+if submit:
+    # write model response
+    st.write(getllamaresponse(input_text, no_words, blog_style))
+    
+
 
 
